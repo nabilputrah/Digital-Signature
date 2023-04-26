df3e95328a928618ae18e7a4ead8e7f87fd45965
nabilputrah <nabilputra28.ns@gmail.com>
2023-04-26 17:14:29 +0700

    [add] try add dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DashboardKoor from '../views/DashboardKoor.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardKoor
   },
   {
     path: '/about',
