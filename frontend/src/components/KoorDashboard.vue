405a2e3d6166022442756e0bc8c0a2b48a45018e
nabilputrah <nabilputra28.ns@gmail.com>
2023-05-12 17:31:44 +0700

    [fix] modul delete KoTA

--- frontend/src/components/KoorDashboard.vue
+++ frontend/src/components/KoorDashboard.vue
@@ -1,28 +1,20 @@
 <template>
-    <!-- Header Component -->
-    <!-- <v-app-bar app color="primary">
-      <v-toolbar-title>My Website</v-toolbar-title>
-    </v-app-bar> -->
-
-    <!-- Sidebar Component -->
-    <!-- <v-navigation-drawer app color="white" width="240">
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer> -->
-
     <!-- Main Content Area -->
     <v-main>
-      <v-container fluid>
-        <h1>Welcome to my website!</h1>
-        <p>Here is some content for the main area of the page.</p>
+      <v-container>
+        <v-text-field
+          v-model="fileName"
+          label="Drop PDF File Here"
+          outlined
+          readonly
+        ></v-text-field>
+        <input
+          ref="fileInput"
+          type="file"
+          accept="application/pdf"
+          @change="handleFileUpload"
+          style="display: none"
+        />
       </v-container>
     </v-main>
 </template>
@@ -30,12 +22,26 @@
 <script>
 export default {
   data: () => ({
-    items: [
-      { title: 'Home', icon: 'mdi-home' },
-      { title: 'About', icon: 'mdi-information' },
-      { title: 'Contact', icon: 'mdi-email' },
-    ],
+    fileName: '',
   }),
+
+  methods: {
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file && file.type === 'application/pdf') {
+        this.fileName = file.name;
+        // Lakukan sesuatu dengan file PDF yang diunggah, misalnya memprosesnya atau mengirimkannya ke server
+        // Di sini, Anda dapat menambahkan logika untuk memproses file PDF yang diunggah sesuai kebutuhan aplikasi Anda.
+        console.log('File PDF yang diunggah:', file);
+      } else {
+        this.fileName = '';
+        console.error('File yang diunggah harus berupa file PDF');
+      }
+    },
+    openFileDialog() {
+      this.$refs.fileInput.click();
+    }
+  }
 }
 </script>
 
