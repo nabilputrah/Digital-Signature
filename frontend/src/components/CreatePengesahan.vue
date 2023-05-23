9275bfb825072360ba6681e60f6015dc9481b28c
nabilputrah <nabilputra28.ns@gmail.com>
2023-05-23 20:28:37 +0700

    [fix] dinamis lembar pengesahan

--- frontend/src/components/CreatePengesahan.vue
+++ frontend/src/components/CreatePengesahan.vue
@@ -6,7 +6,7 @@
       <img src="../assets/logo-polban-81.png" alt="Background Image" class="background-image">
       <div style="text-align: center;margin-top: 79pt;margin-left: 18pt;">
         <strong style="font-size: 13px;line-height: 0.47cm;word-spacing: 0.01cm;">
-          <p v-html="Judul_TA"></p>
+          <p v-html="laporan.judul_TA"></p>
         </strong>
         <br>
         <p class="text-size">Oleh:</p>
@@ -32,7 +32,7 @@
         <br>
         <p class="text-size">Menyetujui</p>
         <br>
-        <p class="text-size">Bandung {{ formattedDate }}</p>
+        <p class="text-size">Bandung {{ this.laporan.tgl_disetujui }}</p>
         <br>
         <v-row class="text-size" style="width: 80%;margin-left: auto;margin-right: auto;">
           <v-col >
@@ -98,10 +98,16 @@
 import jsPDF from 'jspdf';
 import { format, parseISO } from 'date-fns';
 import idLocale from 'date-fns/locale/id';
+import axios from 'axios'
 
 export default {
   data: () => ({
+      dataFromToken: '',
       Judul_TA: '<p>PENGEMBANGAN SISTEM <em>MULTI-USER DIGITAL</em></p><p><em>SIGNATURE</em> UNTUK LAPORAN TUGAS AKHIR&nbsp;</p><p>DENGAN METODE<em> SECRET</em></p><p><em>SHARING SCHEME</em></p>',
+      kota:'',
+      laporan:'',
+      pembimbingKoTA:[],
+
       Anggota : [
         {nama:'Andika Yudha Riyanto', NIM:'191524034'},
         {nama:'Nabil Putra Hadiyani', NIM:'191524053'}
@@ -124,15 +130,63 @@ export default {
       tanggal_disetujui: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
     }),
 
-  computed: {
-    formattedDate() {
-      const parsedDate = parseISO(this.tanggal_disetujui);
-      const formatted = format(parsedDate, 'd MMMM yyyy', { locale: idLocale });
-      return formatted;
-    }
+  mounted () {
+     const token = localStorage.getItem('token');
+
+      if (token) {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        this.dataFromToken= payload.user;
+      }
+    this.initialize()
   },
 
   methods: {
+    async initialize () {
+
+    try {
+        const response = await axios.get(`http://localhost:3000/api/getkotadata/${this.dataFromToken.id_user}`)
+        this.kota = response.data.data[0]
+        console.log(this.kota)
+        const id_kota = response.data.data[0].id_KoTA
+
+        const responseListLaporan = await axios.get('http://localhost:3000/api/laporankota/' +id_kota)
+        this.laporan = responseListLaporan.data.data
+        console.log(this.laporan)
+
+        const responsePembimbing = await axios.get('http://localhost:3000/api/relasibykota/pembimbing/'+ this.laporan.id_KoTA)
+        this.Pembimbing = responsePembimbing.data.data
+        console.log(this.Pembimbing)
+
+        const responseRelasi = await axios.get('http://localhost:3000/api/relasi/KoTA/' + id_kota)
+        this.Pengampu = responseRelasi.data.data
+        console.log(this.Pengampu)
+
+        // const responseKajur = await axios.get('http://localhost:3000/api/jurusan')
+        // this.Kajur = responseKajur.data.data[0]
+        // const DataKajur = await axios.get('http://localhost:3000/api/dosen/'+ this.Kajur.NIP,)
+        // console.log(DataKajur.data.data)
+        // this.Kajur = DataKajur.data.data
+
+        this.convertDateDisetujui()
+        // this.convertDateDisidangkan()
+
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+
+    convertDateDisetujui() {
+      const date = new Date(this.laporan.tgl_disetujui);
+      const year = date.toISOString().substring(0, 4);
+      const month = date.toISOString().substring(5, 7);
+      const day = date.toISOString().substring(8, 10);
+      this.laporan.tgl_disetujui = year + '-' + month + '-' + day;
+      const parsedDate = parseISO(this.laporan.tgl_disetujui);
+      const formatted = format(parsedDate, 'd MMMM yyyy', { locale: idLocale });
+      this.laporan.tgl_disetujui = formatted
+     
+    },
+
     generatePDF() {
       const doc = new jsPDF('p', 'pt', [612, 792]);
       
